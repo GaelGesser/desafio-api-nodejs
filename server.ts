f0522f76c57b53703c89fd45fbaa3b2c93a2b130
GaelGesser <gabrielhenrig@outlook.com>
2025-08-12 09:27:57 -0300

    start: aula 01

---
+++ server.ts
@@ -0,0 +1,53 @@
+import fastify from 'fastify'
+import crypto from 'node:crypto'
+
+const server = fastify({
+  logger: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname'
+      }
+    }
+  }
+})
+
+const cousers = [
+  { id: '1', name: 'John Doe' },
+  { id: '2', name: 'Jane Smith' },
+  { id: '3', name: 'Alice Johnson' },
+  { id: '4', name: 'Bob Brown' },
+]
+
+server.get('/coursers', async (request, reply) => {
+  return reply.status(200).send({ cousers})
+})
+
+server.post('/coursers', async (request, reply) => {
+
+  const { name} = request.body as { name?: string }
+
+  if (!name) {
+    return reply.status(404).send({ message: 'Name is required' })
+  }
+
+  cousers.push({
+    id: crypto.randomUUID(),
+    name: name
+  })
+
+  return reply.status(201).send()
+})
+
+server.get('/coursers/:id', async (request, reply) => {
+  const { id } = request.params as { id: string }
+  const courser = cousers.find(courser => courser.id === id)
+
+  return reply.status(200).send(courser)
+})
+
+server.listen({ port: 3333 }).then(() => {
+  console.log('Server is running on http://localhost:3333')
+})
